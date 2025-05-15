e0a13af0f3ec48b04a1e3ea08dcca1cf42c7eae5
Anton Korobeynikov <anton@korobeynikov.info>
2025-05-15 12:04:54 +0300

    Flatten scopes
    Signed-off-by: Anton Korobeynikov <anton@korobeynikov.info>

--- lib/Transforms/FlattenCFG.cpp
+++ lib/Transforms/FlattenCFG.cpp
@@ -19,11 +19,59 @@ struct FlattenCFGPass : public P4::P4MLIR::impl::FlattenCFGBase<FlattenCFGPass> 
     void runOnOperation() override;
 };
 
+class ScopeOpFlattening : public mlir::OpRewritePattern<P4HIR::ScopeOp> {
+ public:
+    using OpRewritePattern<P4HIR::ScopeOp>::OpRewritePattern;
+
+    mlir::LogicalResult matchAndRewrite(P4HIR::ScopeOp scopeOp,
+                                        mlir::PatternRewriter &rewriter) const override {
+        mlir::OpBuilder::InsertionGuard guard(rewriter);
+        auto loc = scopeOp.getLoc();
+
+        // Empty scope: just remove it.
+        // TODO: Decide if we'd need to do something with annotated scopes
+        if (scopeOp.isEmpty()) {
+            rewriter.eraseOp(scopeOp);
+            return mlir::success();
+        }
+
+        // Split the current block before the ScopeOp to create the inlining point.
+        auto *currentBlock = rewriter.getInsertionBlock();
+        mlir::Block *afterBlock = rewriter.splitBlock(currentBlock, rewriter.getInsertionPoint());
+        if (scopeOp.getNumResults() > 0) afterBlock->addArguments(scopeOp.getResultTypes(), loc);
+
+        // Inline body region.
+        auto *beforeBody = &scopeOp.getScopeRegion().front();
+        auto *afterBody = &scopeOp.getScopeRegion().back();
+        rewriter.inlineRegionBefore(scopeOp.getScopeRegion(), afterBlock);
+
+        // Save stack and then branch into the body of the region.
+        rewriter.setInsertionPointToEnd(currentBlock);
+        rewriter.create<P4HIR::BrOp>(loc, mlir::ValueRange(), beforeBody);
+
+        // Replace the scope return with a branch that jumps out of the body.
+        rewriter.setInsertionPointToEnd(afterBody);
+        if (auto yieldOp = dyn_cast<P4HIR::YieldOp>(afterBody->getTerminator())) {
+            rewriter.replaceOpWithNewOp<P4HIR::BrOp>(yieldOp, yieldOp.getArgs(), afterBlock);
+        }
+
+        // Replace the op with values return from the body region.
+        rewriter.replaceOp(scopeOp, afterBlock->getArguments());
+
+        return mlir::success();
+    }
+};
+
 void FlattenCFGPass::runOnOperation() {
     RewritePatternSet patterns(&getContext());
 
+    patterns.add<ScopeOpFlattening>(patterns.getContext());
+
     // Collect operations to apply patterns.
     llvm::SmallVector<Operation *, 16> ops;
+    getOperation()->walk<mlir::WalkOrder::PostOrder>([&](Operation *op) {
+        if (isa<P4HIR::ScopeOp>(op)) ops.push_back(op);
+    });
 
     // Apply patterns.
     if (applyOpPatternsGreedily(ops, std::move(patterns)).failed()) signalPassFailure();
