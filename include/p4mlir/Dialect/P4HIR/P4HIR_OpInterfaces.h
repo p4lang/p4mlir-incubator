5f3bc5efa4087e867ab587bcfb332f1fc79211b4
Anton Korobeynikov <anton@korobeynikov.info>
2025-05-26 07:26:06 -0700

    Add Annotated interface
    Signed-off-by: Anton Korobeynikov <anton@korobeynikov.info>

---
+++ include/p4mlir/Dialect/P4HIR/P4HIR_OpInterfaces.h
@@ -0,0 +1,8 @@
+#ifndef P4MLIR_DIALECT_P4HIR_P4HIR_OPINTERFACES_H
+#define P4MLIR_DIALECT_P4HIR_P4HIR_OPINTERFACES_H
+
+#include "mlir/IR/OpDefinition.h"
+
+#include "p4mlir/Dialect/P4HIR/P4HIR_OpInterfaces.h.inc"
+
+#endif  // P4MLIR_DIALECT_P4HIR_P4HIR_OPINTERFACES_H
