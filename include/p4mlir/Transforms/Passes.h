f8527f7a806a9b7ff4efa006b4fdf2c07484689b
Manolis Tsamis <tsamismanolis@gmail.com>
2025-08-08 16:06:47 +0300

    Add new optimization pass RemoveParserControlFlow
    Signed-off-by: Manolis Tsamis <tsamismanolis@gmail.com>

--- include/p4mlir/Transforms/Passes.h
+++ include/p4mlir/Transforms/Passes.h
@@ -26,6 +26,7 @@ namespace P4::P4MLIR {
 #define GEN_PASS_DECL_SIMPLIFYPARSERS
 #define GEN_PASS_DECL_SERENUMELIMINATION
 #define GEN_PASS_DECL_REMOVEALIASES
+#define GEN_PASS_DECL_REMOVEPARSERCONTROLFLOW
 #include "p4mlir/Transforms/Passes.h.inc"
 
 std::unique_ptr<mlir::Pass> createPrintParsersGraphPass();
@@ -33,6 +34,7 @@ std::unique_ptr<mlir::Pass> createSimplifyParsersPass();
 std::unique_ptr<mlir::Pass> createFlattenCFGPass();
 std::unique_ptr<mlir::Pass> createSerEnumEliminationPass();
 std::unique_ptr<mlir::Pass> createRemoveAliasesPass();
+std::unique_ptr<mlir::Pass> createRemoveParserControlFlowPass();
 
 #define GEN_PASS_REGISTRATION
 #include "p4mlir/Transforms/Passes.h.inc"
