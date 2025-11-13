bc1090027a8607c1e52237f189623988ecf10ca2
Pietro Ghiglio <pghiglio@accesssoftek.com>
2025-11-13 17:00:00 +0100

    Add initial implementation for BMv2 headers and parsers
    Signed-off-by: Pietro Ghiglio <pghiglio@accesssoftek.com>

--- include/p4mlir/Dialect/BMv2IR/BMv2IR_Types.h
+++ include/p4mlir/Dialect/BMv2IR/BMv2IR_Types.h
@@ -3,9 +3,34 @@
 
 // We explicitly do not use push / pop for diagnostic in
 // order to propagate pragma further on
+#include "llvm/ADT/APFloat.h"
+#include "llvm/Support/raw_ostream.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
 #include "mlir/IR/BuiltinTypes.h"
+
+namespace P4::P4MLIR::BMv2IR {
+
+// Struct that models a header field info.
+struct FieldInfo {
+    mlir::StringAttr name;
+    mlir::Type type;
+
+    FieldInfo(mlir::StringAttr name, mlir::Type type) : name(name), type(type) {}
+
+    bool operator==(const FieldInfo &other) const {
+        return name == other.name && type == other.type;
+    }
+
+    friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os, const FieldInfo &f) {
+        return os << f.name.data() << ":" << f.type;
+    }
+
+    friend llvm::hash_code hash_value(P4::P4MLIR::BMv2IR::FieldInfo f);
+};
+
+}  // namespace P4::P4MLIR::BMv2IR
 
 #define GET_TYPEDEF_CLASSES
 #include "p4mlir/Dialect/BMv2IR/BMv2IR_Types.h.inc"
--- lib/Dialect/BMv2IR/BMv2IR_Types.cpp
+++ lib/Dialect/BMv2IR/BMv2IR_Types.cpp
@@ -1,9 +1,97 @@
 #include "p4mlir/Dialect/BMv2IR/BMv2IR_Types.h"
 
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/LogicalResult.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/Support/LLVM.h"
 #include "p4mlir/Dialect/BMv2IR/BMv2IR_Dialect.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_Types.h"
 
 using namespace mlir;
 using namespace P4::P4MLIR;
+
+template <>
+struct mlir::FieldParser<P4::P4MLIR::BMv2IR::FieldInfo> {
+    static FailureOr<P4::P4MLIR::BMv2IR::FieldInfo> parse(AsmParser &parser) {
+        StringRef name;
+
+        if (failed(parser.parseKeyword(&name))) return failure();
+        if (parser.parseColon()) return failure();
+        Type ty;
+        if (parser.parseType(ty)) return failure();
+
+        return BMv2IR::FieldInfo(StringAttr::get(parser.getContext(), name), ty);
+    }
+};
+
+constexpr unsigned bitsInByte = 8;
+static unsigned computeTotalHeaderLenghtInBits(ArrayRef<BMv2IR::FieldInfo> fields) {
+    unsigned total = 0;
+    for (const auto &field : fields) {
+        total += TypeSwitch<Type, unsigned>(field.type)
+                     .Case<P4HIR::BitsType>([](P4HIR::BitsType bitTy) { return bitTy.getWidth(); })
+                     .Case<P4HIR::VarBitsType>(
+                         [](P4HIR::VarBitsType varBitTy) { return varBitTy.getMaxWidth(); })
+                     .Default([](auto) -> unsigned {
+                         llvm_unreachable("Unsupported field in BMv2 header");
+                     });
+    }
+    return total;
+}
+
+bool BMv2IR::HeaderType::isAllowedFieldType(Type ty) {
+    return isa<P4HIR::BitsType, P4HIR::VarBitsType>(ty);
+}
+
+llvm::LogicalResult BMv2IR::HeaderType::verify(
+    ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError, ::llvm::StringRef name,
+    ::llvm::ArrayRef<BMv2IR::FieldInfo> fields, unsigned max_length) {
+    if (llvm::any_of(fields, [](BMv2IR::FieldInfo field) {
+            return !BMv2IR::HeaderType::isAllowedFieldType(field.type);
+        })) {
+        emitError() << "Only bits and varbits are allowed in BMv2 headers";
+        return failure();
+    }
+
+    unsigned lenInBits = computeTotalHeaderLenghtInBits(fields);
+    if (lenInBits % bitsInByte != 0) {
+        emitError() << "Expected total size of a header to be byte-sized";
+        return failure();
+    }
+
+    unsigned numVarBits = llvm::count_if(
+        fields, [](BMv2IR::FieldInfo field) { return isa<P4HIR::VarBitsType>(field.type); });
+    if (numVarBits > 1) {
+        emitError() << "Expected at most one field with dynamic size.";
+        return failure();
+    }
+
+    auto computedMaxLength = computeMaxLength(fields);
+    if (max_length != computedMaxLength) {
+        emitError() << "Max length mismatch: expected " << computedMaxLength << " got "
+                    << max_length;
+        return failure();
+    }
+
+    return success();
+}
+
+unsigned BMv2IR::HeaderType::computeMaxLength(ArrayRef<BMv2IR::FieldInfo> fields) {
+    unsigned lenInBits = computeTotalHeaderLenghtInBits(fields);
+    return lenInBits / bitsInByte;
+}
+
+#define GET_TYPEDEF_CLASSES
+#include "p4mlir/Dialect/BMv2IR/BMv2IR_Types.cpp.inc"
+
+llvm::hash_code P4::P4MLIR::BMv2IR::hash_value(P4::P4MLIR::BMv2IR::FieldInfo f) {
+    return llvm::hash_value(f.name.getValue());
+}
 
 void BMv2IR::BMv2IRDialect::registerTypes() {
     addTypes<
