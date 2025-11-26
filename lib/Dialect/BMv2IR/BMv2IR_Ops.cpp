f73e14c7aad9c764ae9e2532019ea2a804f03b2d
Pietro Ghiglio <pghiglio@accesssoftek.com>
2025-11-26 21:09:45 +0100

    Add initial support for header instances
    Signed-off-by: Pietro Ghiglio <pghiglio@accesssoftek.com>

--- lib/Dialect/BMv2IR/BMv2IR_Ops.cpp
+++ lib/Dialect/BMv2IR/BMv2IR_Ops.cpp
@@ -1,6 +1,7 @@
 #include "p4mlir/Dialect/BMv2IR/BMv2IR_Ops.h"
 
 #include "mlir/IR/Builders.h"
+#include "p4mlir//Dialect/P4HIR/P4HIR_Types.h"
 #include "p4mlir/Dialect/BMv2IR/BMv2IR_Dialect.h"
 #include "p4mlir/Dialect/BMv2IR/BMv2IR_OpInterfaces.h"
 
@@ -30,6 +31,31 @@ LogicalResult ParserStateOp::verify() {
         }
     }
     return success();
+}
+
+static mlir::ModuleOp getParentModule(Operation *from) {
+    if (auto moduleOp = from->getParentOfType<mlir::ModuleOp>()) return moduleOp;
+
+    from->emitOpError("could not find parent module op");
+    return nullptr;
+}
+
+LogicalResult SymToValueOp::verifySymbolUses(SymbolTableCollection &symbolTable) {
+    // Check that the decl attribute was specified.
+    auto declAttr = (*this)->getAttrOfType<SymbolRefAttr>(getDeclAttrName());
+    if (!declAttr) return emitOpError("requires a 'decl' symbol reference attribute");
+
+    auto decl = symbolTable.lookupSymbolIn(getParentModule(*this), declAttr);
+    if (!decl) return emitOpError("cannot resolve symbol '") << declAttr << "' to declaration";
+
+    if (!mlir::isa<BMv2IR::HeaderInstanceOp>(decl))
+        return emitOpError("invalid symbol reference: ") << decl << ", expected header instance";
+
+    return mlir::success();
+}
+
+void SymToValueOp::getAsmResultNames(OpAsmSetValueNameFn setNameFn) {
+    setNameFn(getResult(), getDecl().getLeafReference());
 }
 
 void BMv2IRDialect::initialize() {
