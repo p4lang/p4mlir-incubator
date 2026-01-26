bc67d8177388578d787dd47de7abc1506aa5387c
Anton Korobeynikov <anton@korobeynikov.info>
2026-01-26 22:21:23 +0300

    Add SROA interfaces
    Signed-off-by: Anton Korobeynikov <anton@korobeynikov.info>

--- lib/Dialect/P4HIR/P4HIR_MemorySlot.cpp
+++ lib/Dialect/P4HIR/P4HIR_MemorySlot.cpp
@@ -1,7 +1,10 @@
-#include "llvm/Support/ErrorHandling.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "mlir/Interfaces/MemorySlotInterfaces.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_Attrs.h"
 #include "p4mlir/Dialect/P4HIR/P4HIR_Dialect.h"
 #include "p4mlir/Dialect/P4HIR/P4HIR_Ops.h"
 #include "p4mlir/Dialect/P4HIR/P4HIR_TypeInterfaces.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_Types.h"
 
 using namespace mlir;
 using namespace P4::P4MLIR;
@@ -29,6 +32,53 @@ void P4HIR::VariableOp::handleBlockArgument(const MemorySlot &slot, BlockArgumen
 std::optional<PromotableAllocationOpInterface> P4HIR::VariableOp::handlePromotionComplete(
     const MemorySlot &slot, Value defaultValue, OpBuilder &builder) {
     if (defaultValue && defaultValue.use_empty()) defaultValue.getDefiningOp()->erase();
+    this->erase();
+    return std::nullopt;
+}
+
+SmallVector<DestructurableMemorySlot> P4HIR::VariableOp::getDestructurableSlots() {
+    auto destructurable = llvm::dyn_cast<DestructurableTypeInterface>(getObjectType());
+    if (!destructurable) return {};
+
+    std::optional<DenseMap<Attribute, Type>> destructuredType =
+        destructurable.getSubelementIndexMap();
+    if (!destructuredType) return {};
+
+    return {DestructurableMemorySlot{{getResult(), getObjectType()}, *destructuredType}};
+}
+
+DenseMap<Attribute, MemorySlot> P4HIR::VariableOp::destructure(
+    const DestructurableMemorySlot &slot, const SmallPtrSetImpl<Attribute> &usedIndices,
+    OpBuilder &builder, SmallVectorImpl<DestructurableAllocationOpInterface> &newAllocators) {
+    builder.setInsertionPointAfter(*this);
+
+    DenseMap<Attribute, MemorySlot> slotMap;
+    SmallVector<Attribute> sortedIndices(usedIndices.begin(), usedIndices.end());
+    llvm::sort(sortedIndices, [](auto a, auto b) {
+        return cast<IntegerAttr>(a).getInt() < cast<IntegerAttr>(b).getInt();
+    });
+
+    auto objectType = llvm::cast<DestructurableTypeInterface>(getObjectType());
+    for (Attribute usedIndex : sortedIndices) {
+        Type elemType = objectType.getTypeAtIndex(usedIndex);
+        assert(elemType && "used index must exist");
+        ReferenceType elemRef = P4HIR::ReferenceType::get(elemType);
+        auto optName = getName();
+        // TODO: Switch to FieldInfo, so we can deduce proper field name
+        auto subVariable = builder.create<P4HIR::VariableOp>(
+            getLoc(), elemRef,
+            optName ? *optName + ".field" + Twine(cast<IntegerAttr>(usedIndex).getInt()) : "",
+            getInit(), getAnnotationsAttr());
+        newAllocators.push_back(subVariable);
+        slotMap.try_emplace<MemorySlot>(usedIndex, {subVariable.getResult(), elemType});
+    }
+
+    return slotMap;
+}
+
+std::optional<DestructurableAllocationOpInterface> P4HIR::VariableOp::handleDestructuringComplete(
+    const DestructurableMemorySlot &slot, OpBuilder &builder) {
+    assert(slot.ptr == getResult());
     this->erase();
     return std::nullopt;
 }
@@ -63,6 +113,54 @@ DeletionKind P4HIR::ReadOp::removeBlockingUses(const MemorySlot &slot,
     return DeletionKind::Delete;
 }
 
+bool P4HIR::ReadOp::canRewire(const DestructurableMemorySlot &slot,
+                              SmallPtrSetImpl<Attribute> &usedIndices,
+                              SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+                              const DataLayout &dataLayout) {
+    if (slot.ptr != getRef()) return false;
+
+    for (auto [idx, _] : slot.subelementTypes) usedIndices.insert(idx);
+
+    return true;
+}
+
+static void getSortedPtrs(DenseMap<Attribute, MemorySlot> &subslots,
+                          SmallVectorImpl<std::pair<unsigned, Value>> &sorted) {
+    for (auto [attr, mem] : subslots) {
+        assert(isa<IntegerAttr>(attr));
+        sorted.emplace_back(cast<IntegerAttr>(attr).getInt(), mem.ptr);
+    }
+
+    llvm::sort(sorted, [](auto a, auto b) { return a.first < b.first; });
+}
+
+DeletionKind P4HIR::ReadOp::rewire(const DestructurableMemorySlot &slot,
+                                   DenseMap<Attribute, MemorySlot> &subslots, OpBuilder &builder,
+                                   const DataLayout &dataLayout) {
+    SmallVector<std::pair<unsigned, mlir::Value>> elements;
+    getSortedPtrs(subslots, elements);
+
+    SmallVector<mlir::Value> vals;
+    for (auto [_, val] : elements) vals.push_back(builder.create<P4HIR::ReadOp>(getLoc(), val));
+
+    Value repl = mlir::TypeSwitch<Type, Value>(getType())
+                     .Case<P4HIR::StructType>([&](auto) {
+                         return builder.create<P4HIR::StructOp>(getLoc(), slot.elemType, vals);
+                     })
+                     .Case<P4HIR::ArrayType>([&](auto) {
+                         return builder.create<P4HIR::ArrayOp>(getLoc(), slot.elemType, vals);
+                     });
+
+    replaceAllUsesWith(repl);
+    return DeletionKind::Delete;
+}
+
+LogicalResult P4HIR::ReadOp::ensureOnlySafeAccesses(const MemorySlot &slot,
+                                                    SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+                                                    const DataLayout &dataLayout) {
+    return success();
+}
+
 //===----------------------------------------------------------------------===//
 // Interfaces for AssignOp
 //===----------------------------------------------------------------------===//
@@ -90,5 +188,134 @@ DeletionKind P4HIR::AssignOp::removeBlockingUses(const MemorySlot &slot,
                                                  const SmallPtrSetImpl<OpOperand *> &blockingUses,
                                                  OpBuilder &builder, Value reachingDefinition,
                                                  const DataLayout &dataLayout) {
+    return DeletionKind::Delete;
+}
+
+bool P4HIR::AssignOp::canRewire(const DestructurableMemorySlot &slot,
+                                SmallPtrSetImpl<Attribute> &usedIndices,
+                                SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+                                const DataLayout &dataLayout) {
+    if (slot.ptr != getRef()) return false;
+    for (auto [idx, _] : slot.subelementTypes) usedIndices.insert(idx);
+
+    return true;
+}
+
+DeletionKind P4HIR::AssignOp::rewire(const DestructurableMemorySlot &slot,
+                                     DenseMap<Attribute, MemorySlot> &subslots, OpBuilder &builder,
+                                     const DataLayout &dataLayout) {
+    SmallVector<std::pair<unsigned, mlir::Value>> elements;
+    getSortedPtrs(subslots, elements);
+
+    mlir::TypeSwitch<Type>(slot.elemType)
+        .Case<StructLikeTypeInterface>([&](auto structType) {
+            for (auto [idx, elt] : elements) {
+                auto &fieldInfo = structType.getFields()[idx];
+                auto val = builder.create<P4HIR::StructExtractOp>(getLoc(), getValue(), fieldInfo);
+                builder.create<P4HIR::AssignOp>(getLoc(), val, elt);
+            }
+        })
+        .Case<ArrayType>([&](auto arrayType) {
+            for (auto [idx, elt] : elements) {
+                auto idxConst = builder.create<P4HIR::ConstOp>(
+                    getLoc(),
+                    P4HIR::IntAttr::get(P4HIR::BitsType::get(getContext(), 32, false), idx));
+                auto val = builder.create<P4HIR::ArrayGetOp>(getLoc(), getValue(), idxConst);
+                builder.create<P4HIR::AssignOp>(getLoc(), val, elt);
+            }
+        });
+
+    return DeletionKind::Delete;
+}
+
+LogicalResult P4HIR::AssignOp::ensureOnlySafeAccesses(const MemorySlot &slot,
+                                                      SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+                                                      const DataLayout &dataLayout) {
+    return success();
+}
+//===----------------------------------------------------------------------===//
+// Interfaces for StructFieldRefOp
+//===----------------------------------------------------------------------===//
+
+bool P4HIR::StructFieldRefOp::canRewire(const DestructurableMemorySlot &slot,
+                                        SmallPtrSetImpl<Attribute> &usedIndices,
+                                        SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+                                        const DataLayout &dataLayout) {
+    if (slot.ptr != getInput()) return false;
+
+    auto indexAttr = IntegerAttr::get(IndexType::get(getContext()), getFieldIndex());
+
+    if (!slot.subelementTypes.contains(indexAttr)) return false;
+
+    usedIndices.insert(indexAttr);
+    mustBeSafelyUsed.emplace_back<MemorySlot>({getResult(), getResult().getType().getObjectType()});
+
+    return true;
+}
+
+DeletionKind P4HIR::StructFieldRefOp::rewire(const DestructurableMemorySlot &slot,
+                                             DenseMap<Attribute, MemorySlot> &subslots,
+                                             OpBuilder &builder, const DataLayout &dataLayout) {
+    auto indexAttr = IntegerAttr::get(IndexType::get(getContext()), getFieldIndex());
+
+    auto it = subslots.find(indexAttr);
+    assert(it != subslots.end());
+
+    replaceAllUsesWith(it->getSecond().ptr);
+
+    return DeletionKind::Delete;
+}
+
+LogicalResult P4HIR::StructFieldRefOp::ensureOnlySafeAccesses(
+    const MemorySlot &slot, SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+    const DataLayout &dataLayout) {
+    return success();
+}
+
+//===----------------------------------------------------------------------===//
+// Interfaces for ArrayElementRefOp
+//===----------------------------------------------------------------------===//
+
+// TODO: Ensure access is in-bounds
+LogicalResult P4HIR::ArrayElementRefOp::ensureOnlySafeAccesses(
+    const MemorySlot &slot, SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+    const DataLayout &dataLayout) {
+    return success();
+}
+
+bool P4HIR::ArrayElementRefOp::canRewire(const DestructurableMemorySlot &slot,
+                                         SmallPtrSetImpl<Attribute> &usedIndices,
+                                         SmallVectorImpl<MemorySlot> &mustBeSafelyUsed,
+                                         const DataLayout &dataLayout) {
+    if (slot.ptr != getInput()) return false;
+
+    // Cano only rewire constant indices
+    auto cstIndex = dyn_cast<P4HIR::ConstOp>(getIndex().getDefiningOp());
+    ;
+    if (!cstIndex) return false;
+
+    auto indexAttr = IntegerAttr::get(IndexType::get(getContext()),
+                                      cstIndex.getValueAs<P4HIR::IntAttr>().getUInt());
+
+    if (!slot.subelementTypes.contains(indexAttr)) return false;
+
+    usedIndices.insert(indexAttr);
+    mustBeSafelyUsed.emplace_back<MemorySlot>({getResult(), getResult().getType().getObjectType()});
+
+    return true;
+}
+
+DeletionKind P4HIR::ArrayElementRefOp::rewire(const DestructurableMemorySlot &slot,
+                                              DenseMap<Attribute, MemorySlot> &subslots,
+                                              OpBuilder &builder, const DataLayout &dataLayout) {
+    auto cstIndex = cast<P4HIR::ConstOp>(getIndex().getDefiningOp());
+    auto indexAttr = IntegerAttr::get(IndexType::get(getContext()),
+                                      cstIndex.getValueAs<P4HIR::IntAttr>().getUInt());
+
+    auto it = subslots.find(indexAttr);
+    assert(it != subslots.end());
+
+    replaceAllUsesWith(it->getSecond().ptr);
+
     return DeletionKind::Delete;
 }
